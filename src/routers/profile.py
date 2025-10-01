608bdcd19a33d0f6d7e82c8069335b380f331a5e
ckirby98 <colin@kirby.net>
2025-10-01 16:30:25 -0400

    added region data api

--- src/routers/profile.py
+++ src/routers/profile.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from repository.profile_repository import fetch_county, fetch_municipality
+from repository.profile_repository import fetch_county, fetch_municipality, fetch_region
 from fastapi_cache.decorator import cache
 
 router = APIRouter(
@@ -16,4 +16,9 @@ async def get_municipality(geoid: str):
 @router.get("/county/{geoid}")
 async def get_county(geoid: str):
     profile = await fetch_county(geoid)
+    return profile
+
+@router.get("/region")
+async def get_region():
+    profile = await fetch_region()
     return profile
