288e287607977b5cbd2266532575ca6db557878d
ckirby98 <colin@kirby.net>
2025-12-03 13:42:29 -0500

    fixed revalide url

--- src/services/revalidate.py
+++ src/services/revalidate.py
@@ -5,20 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 REVALIDATE_SECRET = os.getenv("REVALIDATE_SECRET")
-FRONTEND_URL = os.getenv("FRONTEND_URL")
+FRONTEND_REVALIDATE_URL = os.getenv("FRONTEND_REVALIDATE_URL")
 
 log = logging.getLogger(__name__)
+
 
 def revalidate_all():
     for geo_level in ['region', 'county', 'municipality']:
         revalidate_frontend(geo_level)
+
 
 def revalidate_frontend(geo_level: str):
     payload = {"geoLevel": geo_level}
     headers = {"Authorization": f"Bearer {REVALIDATE_SECRET}"}
 
     try:
-        r = requests.post(f"{FRONTEND_URL}/api/revalidate",
+        r = requests.post(FRONTEND_REVALIDATE_URL,
                           headers=headers, json=payload, timeout=10)
         r.raise_for_status()
         log.info("Revalidated:", r.json())
